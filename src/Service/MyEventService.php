c34f2473acb7d68bf3b4d7e38474ab5ea1c99eb1
Nourhane <n.boudaoud@my-groom-service.com>
2023-11-16 10:23:53 +0100

    maj

---
+++ src/Service/MyEventService.php
@@ -0,0 +1,25 @@
+<?php
+// src/Service/MyEventService.php
+namespace App\Service;
+
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
+use App\Event\MyCustomEvent;
+
+class MyEventService
+{
+    private $eventDispatcher;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    public function doSomething()
+    {
+        // ... Votre logique métier ...
+
+        // Déclencher l'événement
+        $event = new MyCustomEvent('Données associées à l\'événement');
+        $this->eventDispatcher->dispatch($event);
+    }
+}
